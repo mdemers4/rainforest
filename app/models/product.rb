bef2585f95e7e2fe38fae8005455d80419b0e691
mitchell demers <mitchelldemers66@gmail.com>
2016-08-30 18:03:10 -0400

    connected the models to belong to other models

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,6 @@ class Product < ApplicationRecord
 		price_in_dollars = price_in_cents.to_f / 100
 		sprintf("%.2f", price_in_dollars)
 	end
+	has_many :reviews
+	has_many :users, through: :reviews
 end
